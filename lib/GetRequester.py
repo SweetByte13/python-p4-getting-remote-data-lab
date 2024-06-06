18dc99586b53b22f0d9062625ca3a01ce4b581a9
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-06-06 15:27:24 -0400

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,14 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self, url='https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'):
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
-        pass
+        resp_list = []
+        json_resps = json.loads(self.get_response_body())
+        for json_resp in json_resps:
+            resp_list.append(json_resp)
+
+        return resp_list
